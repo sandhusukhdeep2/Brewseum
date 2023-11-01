586d81014e208eff30f9ce6a120b40545aba5708
Sukhdeep Sandhu <sandhusukhdeep2@gmail.com>
2023-11-01 12:02:31 +1100

    finel

---
+++ inc/header.php
@@ -0,0 +1,23 @@
+<!-- Logo & Site Name -->
+		<div class="placeholder">
+			<div class="parallax-window" data-parallax="scroll" data-image-src="img/simple-house-01.jpg">
+				<div class="tm-header">
+					<div class="row tm-header-inner">
+						<div class="col-md-6 col-12">
+							<img src="img/simple-house-logo.png" alt="Logo" class="tm-site-logo" /> 
+							<div class="tm-site-text-box">
+								<h1 class="tm-site-title">Brewseum</h1>
+								<h6 class="tm-site-description">new restaurant template</h6>	
+							</div>
+						</div>
+						<nav class="col-md-6 col-12 tm-nav">
+							<ul class="tm-nav-ul">
+								<li class="tm-nav-li"><a href="index.php" class="tm-nav-link active">Home</a></li>
+								<li class="tm-nav-li"><a href="about.php" class="tm-nav-link">About</a></li>
+								<li class="tm-nav-li"><a href="contact.php" class="tm-nav-link">Contact</a></li>
+							</ul>
+						</nav>	
+					</div>
+				</div>
+			</div>
+		</div>
